5541bdee8ce27f25bc09e51479afefb3350a4008
Petr Pavelka <petr.pavelka@student.gyarab.cz>
2023-06-20 20:23:55 +0200

    hopefully ready

--- 2022_wa_pavelka_movies/gamdb/films/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class CommentForm(forms.Form):
-    author = forms.CharField(required=False)
-    text = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 5}))
-    rating = forms.IntegerField(required=False)
